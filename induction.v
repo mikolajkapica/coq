258a1a3ef68d0f9f7c3517bc03d19e478295c176
Mikolaj Kapica <mikolajkapica@mail.com>
2023-09-06 22:05:59 +0200

    Example Proofs by Induction (ep #23)

--- induction.v
+++ induction.v
@@ -9,7 +9,7 @@ Example add_0_l : forall n : nat,
 
 Proof. reflexivity. Qed.
 
-Theorem add_0_r_secondary : forall n : nat,
+Theorem add_0_r_secondtry : forall n : nat,
     n + 0 = n.
 Proof.
     intros n.
@@ -28,5 +28,34 @@ Proof.
     - reflexivity.
     - simpl. rewrite -> IHn'. reflexivity.
 Qed.
+
+Theorem minus_n_n : forall n,
+    n - n = 0.
+Proof.
+    intros n.
+    induction n as [| n' IHn'].
+    - reflexivity.
+    - simpl. rewrite -> IHn'. reflexivity.
+Qed.
+
+Theorem plus_a_Sb: forall a b : nat,
+    S (a + b) = a + S b.
+Proof.
+    intros a b.
+    induction a as [| a' IHa'].
+    - reflexivity.
+    - simpl. rewrite IHa'. reflexivity.
+Qed.
+
+Theorem add_comm : forall a b : nat,
+    a + b = b + a.
+Proof.
+    intros a b.
+    induction a as [| a' IHa'].
+    - simpl. rewrite add_0_r. reflexivity.
+    - simpl. rewrite -> IHa'. rewrite -> plus_a_Sb. reflexivity.
+Qed.
+
+
 
 
